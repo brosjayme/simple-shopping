4381298d10e2c7a68739502fab5c3d67dc800a4d
unknown <brosjayme@gmail.com>
2023-05-14 15:46:59 +0100

    first php commit

--- admin-page.php
+++ admin-page.php
@@ -2,14 +2,25 @@
 @include 'config.php';
 
 if(isset($_POST['add_product'])){
+
     $product_name = $_POST['product_name'];
     $product_price = $_POST['product_price'];
-    $product_image = $_POST['product_imaage']['name'];
-    $product_image_tmp_name = $_POST['product_imaage']['tmp_name'];
+    $product_image = $_FILES['product_image']['name'];
+    $product_image_tmp_name = $_FILES['product_image']['tmp_name'];
     $product_image_folder = 'uploaded_img/'.$product_image;
 
 if(empty($product_name) || empty($product_price) || empty($product_image)){
-    $message[] = 'cannot be blank';
+    $message[] = 'please fill out all fields';
+} else{
+    $insert = "INSERT INTO products(name, price, image) VALUES('$product_name', '$product_price',
+     '$product_image')";
+     $upload = mysqli_query($conn, $insert);
+     if($upload){
+        move_uploaded_file($product_image_tmp_name, $product_image_folder);
+        $message[] = 'New product added successfully';
+     }else{
+        $message[] = 'new product upload failed';
+     }
 }
 }
 
@@ -31,6 +42,16 @@ if(empty($product_name) || empty($product_price) || empty($product_image)){
 <link rel="stylesheet" href="style.css">
 </head>
 <body>
+    <?php
+
+   if(isset($message)){
+    foreach($message as $message){
+     echo '<span class="message">'.$message.'</span>';
+    }
+   }
+
+    ?>
+
  <div class="container">
     <div class="admin-product-form">
 <form action="?php $_SERVER['PHP_SELF'] ?" method="post" enctype="multipart/form-data">
@@ -41,6 +62,12 @@ if(empty($product_name) || empty($product_price) || empty($product_image)){
 <input type="submit" class="btn" name="add_product" value="add product">
 </form>
     </div>
+
+    <?php
+$select = mysqli_query($conn, "SELECT * FROM products");
+
+ ?>
+<div class="product-display"></div>
  </div>  
  
 </body>
